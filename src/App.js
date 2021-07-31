d3ec9b1ffec35e65a2af135224d61a61f8bed101
Desiree Nelson <palebluedot4@gmail.com>
2021-07-30 23:33:44 -0400

    removed footer from App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import { Container, Col, Row, Button } from 'react-bootstrap';
-import { War, Header, Footer } from './components';
+import { War, Header } from './components';
 
 function App() {
   const [game, setGame] = useState('off');
