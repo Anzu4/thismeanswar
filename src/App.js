1c9cff76d8b30059f1d09b44cf2964c8c9f8c20f
Desiree Nelson <palebluedot4@gmail.com>
2021-07-30 23:14:12 -0400

    updated css, moved some boxes around. Added backgrounds

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
-import { Container, Row, Button } from 'react-bootstrap';
+import { Container, Col, Row, Button } from 'react-bootstrap';
 import { War, Header, Footer } from './components';
 
 function App() {
@@ -9,42 +9,51 @@ function App() {
   if (game === 'off') {
     return (
       <div>
-        <Container className={'App'}>
-          <Row>
+        <Container className={'App'} fluid>
+          <Row id={'site_header'}>
             <Header />
           </Row>
-          <Row>
-            <div className={'splash_text_box'}>
-              <p>'An ancient and brutal fight to the death!</p>
-              <p>
-                You and your opponent each recieve 26 cards. On each turn, you
-                will each flip your top card. Highest card will capture both
-                cards!
-              </p>
-              <p>
-                If there is a tie, you will be placed in an epic battle where
-                the top 2 cards are placed face-down, and the next card is
-                placed face-up. Highest of the face-up cards captures all the
-                cards from that round.
-              </p>
-              First player to obtain all 52 cards will be declared VICTORIOUS'
-            </div>
-          </Row>
-
-          <Button className={'button'} onClick={() => setGame('on')}>
-            Play WAR
-          </Button>
-          <Row>
-            <div className={'team_text_box'}>
-              <p>
-                Solo project by Desiree Nelson for the MintBean Hiring
-                Hackathon.
-              </p>
-              Made with create-react-app and Javascript. Deployed on Netlify!
-            </div>
+          <Row id={'splash-main'}>
+            <Col className={'splash_text_box'}>
+              <div className={'splash-text'}>
+                <p>An ancient and brutal fight to the death!</p>
+                <p>
+                  You and your opponent each recieve 26 cards. On each turn, you
+                  will each flip your top card. Highest card will capture both
+                  cards!
+                </p>
+                <p>
+                  If there is a tie, you will be placed in an epic battle where
+                  the top 2 cards are placed face-down, and the next card is
+                  placed face-up. Highest of the face-up cards captures all the
+                  cards from that round.
+                </p>
+                <p>
+                  First player to obtain all 52 cards will be declared
+                  VICTORIOUS!!
+                </p>
+                <div>
+                  <Button
+                    className={'splash-button'}
+                    onClick={() => setGame('on')}
+                  >
+                    Play WAR
+                  </Button>
+                </div>
+                <div className={'splash-text'}>
+                  <p>
+                    Solo project by Desiree Nelson for the MintBean Hiring
+                    Hackathon.
+                  </p>
+                  <p>
+                    Made with create-react-app and Javascript. Deployed on
+                    Netlify!
+                  </p>
+                </div>
+              </div>
+            </Col>
           </Row>
         </Container>
-        <Footer />
       </div>
     );
   } else {
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function Header() {
-  return <h1 id={'site_header'}>This Means WAR!</h1>;
+  return <h1 >This Means WAR!</h1>;
 }
