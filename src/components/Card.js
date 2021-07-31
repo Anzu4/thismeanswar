e8aad582965caa02fecb3796c1b0b870436b34bc
Desiree Nelson <palebluedot4@gmail.com>
2021-07-31 07:21:05 -0400

    added icons for card suits. Rounded corners on cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,12 +3,43 @@ import '../App.css';
 
 export default function Card({ card }) {
   if (card) {
-    return (
-      <div className={'single-card'}>
-        <p className={'card-suit'}>{card.Suit}</p>
-        <p className={'card-value'}>{card.Rank}</p>
-      </div>
-    );
+    if (card.Suit === 'Hearts') {
+      return (
+        <div className={'single-card'}>
+          <p className={'card-suit-red'}>
+            <i className='fas fa-heart'></i>
+          </p>
+          <p className={'card-value-red'}>{card.Rank}</p>
+        </div>
+      );
+    } else if (card.Suit === 'Diamonds') {
+      return (
+        <div className={'single-card'}>
+          <p className={'card-suit-red'}>
+            <i className='fas fa-gem'></i>
+          </p>
+          <p className={'card-value-red'}>{card.Rank}</p>
+        </div>
+      );
+    } else if (card.Suit === 'Spades') {
+      return (
+        <div className={'single-card'}>
+          <p className={'card-suit-black'}>
+            <i className='fas fa-spa'></i>
+          </p>
+          <p className={'card-value-black'}>{card.Rank}</p>
+        </div>
+      );
+    } else if (card.Suit === 'Clubs') {
+      return (
+        <div className={'single-card'}>
+          <p className={'card-suit-black'}>
+            <i class='fas fa-horse-head'></i>
+          </p>
+          <p className={'card-value-black'}>{card.Rank}</p>
+        </div>
+      );
+    }
   } else {
     return (
       <div>
