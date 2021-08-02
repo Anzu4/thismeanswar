63118c99f7d05f9115cf40ed387a14515fa98916
Desiree Nelson <palebluedot4@gmail.com>
2021-08-02 09:23:33 -0400

    tidied code for deployment

--- src/components/Lose.js
+++ src/components/Lose.js
@@ -2,18 +2,18 @@ import React, { useState } from 'react';
 import War from './War';
 
 export default function Lose() {
-  const [game, setGame] = useState('off');
+  const [reset, setReset] = useState('off');
 
-  if (game === 'off') {
+  if (reset === 'off') {
     return (
       <div className={'lose-page'}>
         <div className={'win-lose-box'}>
           <p>The enemy's forces have overwhelmed you.</p>
-          <button>Play Again?</button>
+          <button onClick={setReset('on')}>Play Again?</button>
         </div>
       </div>
     );
-  } else {
+  } else if (reset === 'on') {
     return <War />;
   }
 }
